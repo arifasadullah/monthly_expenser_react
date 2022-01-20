fc7fcf33fa23a8647df83b5fc7e2e19c8f6bfa54
arifasadullah123 <arifasadullah123@gmai.com>
2022-01-20 11:30:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Expenses from "./Component/Expenses/Expenses";
+import NewExpenses from "./Component/NewExpenses/NewExpense";
+
+const DUMMY_DATA = [
+  {
+    id: "e1",
+    title: "Rosogullah",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "Mac Pro", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "kawasaki Ninja",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "DSLR",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+function App() {
+  const [expenses, setExpenses] = useState(DUMMY_DATA);
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  return (
+    <div>
+      <NewExpenses onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Expenses/Expenses.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesChart from "./ExpensesChart";
+import "./Expenses.css";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2021");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+  return (
+    <Card className="expenses">
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      <ExpensesChart expenses={filteredExpenses} />
+      {filteredExpenses.length === 0 ? (
+        <p>No Expense Found</p>
+      ) : (
+        filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))
+      )}
+      {/* {filteredExpenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))} */}
+    </Card>
+  );
+}
+export default Expenses;
