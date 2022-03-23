0e72f23336597332698a1907fa7144b07d56afb0
Rabia808 <arifasadullah123@gmai.com>
2022-03-23 15:48:13 +0530

    color changes

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import Expenses from "./Component/Expenses/Expenses";
 import NewExpenses from "./Component/NewExpenses/NewExpense";
 
 const DUMMY_DATA = [
-  // {
-  //   id: "e1",
-  //   title: "Rosogullah",
-  //   amount: 94.12,
-  //   date: new Date(2020, 7, 14),
-  // },
-  // { id: "e2", title: "Mac Pro", amount: 799.49, date: new Date(2021, 2, 12) },
-  // {
-  //   id: "e3",
-  //   title: "kawasaki Ninja",
-  //   amount: 294.67,
-  //   date: new Date(2021, 2, 28),
-  // },
-  // {
-  //   id: "e4",
-  //   title: "DSLR",
-  //   amount: 450,
-  //   date: new Date(2021, 5, 12),
-  // },
+  {
+    id: "e1",
+    title: "Rosogullah",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "Mac Pro", amount: 799.49, date: new Date(2021, 7, 12) },
+  {
+    id: "e3",
+    title: "kawasaki Ninja",
+    amount: 294.67,
+    date: new Date(2021, 7, 28),
+  },
+  {
+    id: "e4",
+    title: "DSLR",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
 ];
 
 function App() {
--- src/Component/Chart/ChartBar.js
+++ src/Component/Chart/ChartBar.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./ChartBar.css";
 const ChartBar = (props) => {
-  let barFilledHeight = "0%";
+  let barFillHeight = "0%";
   if (props.maxValue > 0) {
-    barFilledHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
   }
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
         <div
           className="chart-bar__fill"
-          style={{ height: barFilledHeight }}
+          style={{ height: barFillHeight }}
         ></div>
       </div>
       <div className="chart-bar__label">{props.label}</div>
